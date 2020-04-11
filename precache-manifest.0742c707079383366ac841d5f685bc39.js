94444bfb0762dc0e7eb370ec0ff50d985124fdce
anna <sudolann@gmail.com>
2020-04-11 23:33:17 +0200

    Updates

---
+++ precache-manifest.0742c707079383366ac841d5f685bc39.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "63f3de068c1a1d8b5b5d25f7390c8ba8",
+    "url": "/guess-song-game/static/media/dj-mixer.63f3de06.svg"
+  },
+  {
+    "revision": "4aaa7a885a5e3c15661b",
+    "url": "/guess-song-game/static/css/main.1c6cac54.chunk.css"
+  },
+  {
+    "revision": "a6ba5f47967f19e9177f",
+    "url": "/guess-song-game/static/js/runtime~main.9e13b937.js"
+  },
+  {
+    "revision": "241f3e68d70ba1198e53",
+    "url": "/guess-song-game/static/js/2.7236f943.chunk.js"
+  },
+  {
+    "revision": "2b6ee180387749cf1991e5ee1179a444",
+    "url": "/guess-song-game/static/media/badge.2b6ee180.svg"
+  },
+  {
+    "revision": "6ec7b21606c92253dc5f773c98cbf912",
+    "url": "/guess-song-game/static/media/user_no_photo_600x600.6ec7b216.png"
+  },
+  {
+    "revision": "4aaa7a885a5e3c15661b",
+    "url": "/guess-song-game/static/js/main.251d5770.chunk.js"
+  },
+  {
+    "revision": "f563caf55633869573c915dff1507a63",
+    "url": "/guess-song-game/static/media/logo.f563caf5.svg"
+  },
+  {
+    "revision": "9c3474f83561cbb35d832b98bcb35cf2",
+    "url": "/guess-song-game/static/media/play-button.9c3474f8.svg"
+  },
+  {
+    "revision": "bc130bad1fd865d8be50a50c255c0216",
+    "url": "/guess-song-game/static/media/medal-icon.bc130bad.svg"
+  },
+  {
+    "revision": "dea9a7736d8f0694099df67a0be05e2c",
+    "url": "/guess-song-game/static/media/Alba.dea9a773.woff"
+  },
+  {
+    "revision": "d2a4717e921a178a0e2b149cb823bcfe",
+    "url": "/guess-song-game/static/media/AlbaSuper.d2a4717e.woff"
+  },
+  {
+    "revision": "e470258e06a122e39f3b9a0e8782b66f",
+    "url": "/guess-song-game/index.html"
+  }
+];
