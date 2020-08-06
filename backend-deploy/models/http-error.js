68411ac6cd4970ce1a8dc5f808b213577de7afcc
Admin <admin@MacBook-Pro-Admin.local>
2020-08-06 12:04:35 +0200

    removed backend deploy

--- backend-deploy/models/http-error.js
+++
@@ -1,9 +0,0 @@
-class HttpError extends Error {
-  constructor(message, errorCode) {
-    super(message);
-    this.code = errorCode;
-  }
-}
-
-// eslint-disable-next-line no-undef
-module.exports = HttpError;
